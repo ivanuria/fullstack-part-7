25722b019c1bc5e364754dae061e63da7e71741c
ivanuria <ivan.uria@gmail.com>
2024-08-16 15:24:14 +0200

    separate store.js

--- bloglistapp/src/main.jsx
+++ bloglistapp/src/main.jsx
@@ -1,21 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
-
-import notificationReducer from './reducers/notifications'
-import blogsReducer from './reducers/blogs'
-import userReducer from './reducers/user'
-
-const store = configureStore({
-  reducer: {
-    notifications: notificationReducer,
-    blogs: blogsReducer,
-    user: userReducer
-  },
-})
-
-console.log(store.getState())
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
