59cb4ee8bca21eb590b8bfa7273dd7fea3252108
ivanuria <ivan.uria@gmail.com>
2024-08-19 15:56:56 +0200

    Added Home

---
+++ bloglistapp/src/Views/Home.jsx
@@ -0,0 +1,22 @@
+import H2 from "../components/H2"
+import { Typography } from '@mui/material'
+const Home = () => {
+  return (
+    <>
+      <H2>Welcome to BlogApp</H2>
+      <Typography
+        component='p'
+        sx={{
+          marginBlockEnd:'1em'
+        }}
+      >
+        This is the Application realized for the <a href='https://fullstackopen.com/en/'>FullStack Open Course</a> offered by the University of Helsinky.
+      </Typography>
+      <Typography component='p'>
+        This app presents the exercises from part 7 and can be found in <a href='https://github.com/ivanuria/fullstack-part-7'>this github repository</a>. I am fully aware that the Playwright tests are broken since the <em>style changes</em> with <a href='https://mui.com/'>MUI</a>.
+      </Typography>
+    </>
+  )
+}
+
+export default Home
