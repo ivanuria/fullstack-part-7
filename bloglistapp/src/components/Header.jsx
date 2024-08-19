5f6630f183849dddf9e586fbf12ae607370f5fe6
ivanuria <ivan.uria@gmail.com>
2024-08-19 08:45:45 +0200

    Refactor MainMenu

--- bloglistapp/src/components/Header.jsx
+++ bloglistapp/src/components/Header.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { loggedInUser } from '../reducers/user.js'
 import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
@@ -16,68 +15,7 @@ import {
   Typography
 } from '@mui/material'
 
-const MainMenuItem = ({ navItem }) => {
-  const [anchorEl, setAnchorEl] = useState()
-
-  if (navItem.to) {
-    return (
-      <MenuItem
-        component={ Link }
-        to={navItem.to}
-        sx={{
-          fontWeight: '700'
-        }}
-      >
-        {navItem.text}
-      </MenuItem>
-    )
-  }
-  if (navItem.submenu) {
-    return (
-      <>
-      <MenuItem
-        sx={{
-          fontWeight: '700'
-        }}
-        onClick={(e) => setAnchorEl(e.currentTarget)}
-      >
-        {navItem.text}
-      </MenuItem>
-      <Menu
-        anchorEl={anchorEl}
-        open={!!anchorEl}
-        onClose={() => setAnchorEl(null)}
-      >
-        {navItem.submenu.map(navItem =>
-          <MainMenuItem key={navItem.to} navItem={navItem} />
-        )}
-      </Menu>
-      </>
-    )
-  }
-}
-
-const MainMenu = ({ navItems }) => {
-  return (
-    <Box
-      component='nav'
-    >
-      <Box
-        component='menu'
-        key='mainmenu'
-        sx={{
-          paddingInlineStart: '2ch',
-          display: 'flex',
-          listStyleType: 'none'
-        }}
-      >
-        {navItems.map(navItem =>
-          <MainMenuItem key={navItem.to} navItem={navItem} />
-        )}
-      </Box>
-    </Box>
-  )
-}
+import MainMenu from './MainMenu.jsx'
 
 const Header = () => {
   const user = loggedInUser()
