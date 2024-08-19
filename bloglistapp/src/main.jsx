e1c5dc8563a91a5e31f91f0ff1264908a0b7f8ec
ivanuria <ivan.uria@gmail.com>
2024-08-19 09:36:57 +0200

    refactor theme to another file

--- bloglistapp/src/main.jsx
+++ bloglistapp/src/main.jsx
@@ -3,62 +3,8 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import store from './store'
-
-import { ThemeProvider, createTheme } from '@mui/material/styles'
-// fonts
-import '@fontsource/nanum-gothic'
-import '@fontsource/love-ya-like-a-sister'
-
-let theme = createTheme({
-  palette: {
-    primary: {
-      main: 'hsl(280, 100%, 50%)'
-    },
-    secondary: {
-      main: 'hsl(50, 100%, 75%)'
-    }
-  },
-  typography: {
-    fontFamily: 'Nanum Gothic, sans serif',
-    h1: {
-      fontFamily: 'Love Ya Like A Sister'
-    }
-  },
-})
-
-theme = createTheme(theme, {
-  palette: {
-    tertiary: theme.palette.augmentColor({
-      color: {
-        main: 'hsl(110, 100%, 75%)'
-      },
-      name: 'tertiary'
-    })
-  }
-})
-
-theme = createTheme(theme, {
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        'main a': {
-          color: theme.palette.primary.light,
-          fontWeight: 'normal',
-          textDecoration: 'none',
-          backgroundImage: 'linear-gradient(to top, currentcolor 1px, transparent 1px)',
-          padding: 4,
-          margin: -4
-        },
-        'main a:visited': {
-          color: theme.palette.primary.dark
-        },
-        table: {
-          width: '100%'
-        }
-      }
-    }
-  }
-})
+import theme from './theme'
+import { ThemeProvider } from '@mui/material/styles'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider theme={theme}>
---
+++ bloglistapp/src/theme.js
@@ -0,0 +1,57 @@
+import { createTheme } from '@mui/material/styles'
+// fonts
+import '@fontsource/nanum-gothic'
+import '@fontsource/love-ya-like-a-sister'
+
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: 'hsl(280, 100%, 50%)'
+    },
+    secondary: {
+      main: 'hsl(50, 100%, 75%)'
+    }
+  },
+  typography: {
+    fontFamily: 'Nanum Gothic, sans serif',
+    h1: {
+      fontFamily: 'Love Ya Like A Sister'
+    }
+  },
+})
+
+theme = createTheme(theme, {
+  palette: {
+    tertiary: theme.palette.augmentColor({
+      color: {
+        main: 'hsl(110, 100%, 75%)'
+      },
+      name: 'tertiary'
+    })
+  }
+})
+
+theme = createTheme(theme, {
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        'main a': {
+          color: theme.palette.primary.light,
+          fontWeight: 'normal',
+          textDecoration: 'none',
+          backgroundImage: 'linear-gradient(to top, currentcolor 1px, transparent 1px)',
+          padding: 4,
+          margin: -4
+        },
+        'main a:visited': {
+          color: theme.palette.primary.dark
+        },
+        table: {
+          width: '100%'
+        }
+      }
+    }
+  }
+})
+
+export default theme
